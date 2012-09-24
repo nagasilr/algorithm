186efcfb68e2294d2d462b4f7712fd1acb651a6f
guoguo <nagasilr@gmail.com>
2012-09-23 17:24:15 -0700

    Initial submit of Sort

---
+++ Sort.java
@@ -0,0 +1,166 @@
+import java.lang.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.io.*;
+
+/**
+ * Choose among the sort methods "mergesort", "quicksort" or "heapsort"
+ * by specifying the method as command line arguments.
+ * 
+ * The program accepts strings line-by-line as standard input until the end of
+ * input or an empty line and sort the strings ascending order.
+ */
+public class Sort
+{ 
+    public static void quickSort(String[] list) {
+        quickSortInternal(list, 0, list.length - 1);
+    }
+    
+    private static void quickSortInternal(String[] list, int start, int end) {
+
+        if (start == end) {
+	    return;
+        }
+	String pivot = list[start];
+	// Point to the last item that is smaller than the pivot.                                                                                                                                                 
+        int partition = start;
+	for (int index = start + 1; index <= end; index++) {
+	    if (list[index].compareTo(pivot) < 0) {
+		String temp = list[index];
+		list[index] = list[partition + 1];
+		list[partition + 1] = temp;
+		partition++;
+            }
+	}
+        if (partition != start) {
+            list[start] = list[partition];
+	    list[partition] = pivot;
+        }
+	if (start < partition) {
+            quickSortInternal(list, start, partition);
+	}
+	if (partition +1 < end) {
+	    quickSortInternal(list, partition + 1, end);
+	}
+    }
+    
+    public static void mergeSort(String[] list) {
+        mergeSortInternal(list, 0, list.length - 1);
+    }
+    
+    private static void mergeSortInternal(String[] list, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+	int mid = (start + end) >> 1;
+        if (mid > start) {
+            mergeSortInternal(list, start, mid);
+        }
+        if (end > mid + 1) {
+            mergeSortInternal(list, mid + 1, end);
+        }
+        String[] mergedList = new String[end - start + 1];
+        int pointer1 = start, pointer2 = mid + 1, i = 0;
+        while (pointer1 <= mid && pointer2 <= end) {
+            if (list[pointer1].compareTo(list[pointer2]) > 0) {
+                mergedList[i++] = list[pointer2++];
+            } else {
+                mergedList[i++] = list[pointer1++];
+            }
+        }
+        while (pointer1 <= mid) {
+            mergedList[i++] = list[pointer1++];
+        }
+        while (pointer2 <= end) {
+            mergedList[i++] = list[pointer2++];
+        }
+        for (i = start; i <= end; i++) {
+            list[i] = mergedList[i - start];
+        }
+    }
+    
+    public static void heapSort(String[] list) {
+        if (list.length <= 1) {
+            return;
+        }
+        
+        // Generate heap
+        for (int i = 1; i < list.length; i++) {
+            int child = i;
+            // upward heapify
+            while (child > 0) {
+		int parent = ((child + 1) >> 1) - 1;
+                // put break is good?
+		if (list[parent].compareTo(list[child]) < 0) {
+		    String temp = list[parent];
+		    list[parent] = list[child];
+		    list[child] = temp;
+                    child = parent;
+                } else {
+                    break;
+                }
+            }
+        }
+        
+        for (int i = 0; i < list.length - 1; i++) {
+            int parent = 0, boundary = list.length - i - 1;
+            String last = list[boundary];
+	    list[boundary] = list[parent];
+            list[parent] = last;
+            while (true) {
+		int child1 = ((parent + 1) << 1) - 1, child2 = (parent + 1) << 1;
+                if (child1 < boundary && list[child1].compareTo(last) > 0 ||
+		    child2 < boundary && list[child2].compareTo(last) > 0) {
+                    int index = child1;
+                    if (child2 < boundary && list[child1].compareTo(list[child2]) < 0) {
+			index = child2;
+                    }
+                    String temp = list[index];
+                    list[index] = list[parent];
+                    list[parent] = temp;
+                    parent = index;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+    
+    /**
+     * This is the main entry point for the application
+     */
+    public static void main(String args[])
+    {
+        System.out.println("Input strings to be sorted line-by-line");
+        InputStreamReader converter = new InputStreamReader(System.in);
+	BufferedReader in = new BufferedReader(converter);
+        ArrayList<String> input = new ArrayList<String>();
+        String curLine;
+        try {
+	    while((curLine = in.readLine()) != null && curLine.length() != 0) {
+		input.add(curLine);
+	    }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        
+        String[] array = input.toArray(new String[input.size()]);
+        if (args.length > 0) {
+            if (args[0].equals("quicksort")) {
+		quickSort(array);
+            } else if (args[0].equals("mergesort")) {
+                mergeSort(array);
+            } else if (args[0].equals("heapsort")) {
+		heapSort(array);
+            } else {
+                System.out.println(
+				   "Supported sort methods are \"quicksort\", \"mergesort\", \"heapsort\"." +
+				   " Provide one of them as the input!");
+            }
+        }
+	for(String s : array) {
+	    System.out.println(s);
+	}
+    }
+}
+
